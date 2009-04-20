ed1b705814de90bb1f348e30ccf5826d23d08ac7
Pino Toscano <pino@kde.org>
2009-04-20 10:21:22 +0000

    don't miss the .cc files here
    svn path=/trunk/playground/artwork/dekorator/; revision=956581

--- client/Messages.sh
+++ client/Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cpp` -o $podir/kwin3_deKorator.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.cc` -o $podir/kwin3_deKorator.pot
 rm -f rc.cpp
 
